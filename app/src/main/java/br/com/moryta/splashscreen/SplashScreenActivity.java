18ed160dcfcfddfd669140d49c86b4239011becd
Alessandro <alemoryta@gmail.com>
2017-05-11 23:31:40 -0300

    Implementando transição para MenuActivity

--- app/src/main/java/br/com/moryta/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/br/com/moryta/splashscreen/SplashScreenActivity.java
@@ -1,13 +1,50 @@
 package br.com.moryta.splashscreen;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    //Tempo que nosso splashscreen ficará visivel
+    private final int SPLASH_DISPLAY_LENGTH = 3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        load();
+    }
+
+    private void load() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+
+        animation.reset();
+
+        //Pegando o nosso objeto criado no layout
+        ImageView imageView =  (ImageView) findViewById(R.id.splash);
+
+        if (imageView != null) {
+            imageView.clearAnimation();
+            imageView.startAnimation(animation);
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Após o tempo definido irá executar a próxima tela
+                Intent intent = new Intent(SplashScreenActivity.this, MenuActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+
+                startActivity(intent);
+
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
