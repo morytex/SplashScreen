c0fc1aea5f3737f51a5d164a87c1225958c50ea3
Alessandro <alemoryta@gmail.com>
2017-05-20 13:28:14 -0300

    Implementando MainActivity

--- app/src/main/java/br/com/moryta/splashscreen/MainActivity.java
+++ app/src/main/java/br/com/moryta/splashscreen/MainActivity.java
@@ -3,11 +3,11 @@ package br.com.moryta.splashscreen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MenuActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/br/com/moryta/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/br/com/moryta/splashscreen/SplashScreenActivity.java
@@ -1,9 +1,9 @@
 package br.com.moryta.splashscreen;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -38,7 +38,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             @Override
             public void run() {
                 // Após o tempo definido irá executar a próxima tela
-                Intent intent = new Intent(SplashScreenActivity.this, MenuActivity.class);
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 
                 startActivity(intent);
